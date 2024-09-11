6f3a81944e4cb8119297e5c72274a0fb1058aafe
Konsta Kuosmanen <contact@norpie.dev>
2024-09-11 16:39:02 +0200

    docs: remove old comments

--- metrical/test.sh
+++ metrical/test.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-
-# Ik beschouw dat het doel bereikt is als de databasesoftware met succes gegevens opslaat en ophaalt voor minimaal 1.000 datapunten.
-#
-# struct Metric {
-#    name: String,
-#    key: String,
-#    timestamp: u64,
-#    value: f64,
-#}
-
 # get http://localhost:4340/metrics?name=test&key=test
 # post http://localhost:4340/metrics { name: "test", key: "test", timestamp: 1234567890, value: 123.456 }
 
-# Test 1: n datapunten
 n=1000
 for i in $(seq 1 $n); do
     # current timestamp in nanos and random value between 0 and 1000
@@ -23,7 +12,6 @@ for i in $(seq 1 $n); do
     echo "POST $i/$n"
 done
 
-# Test 2: ophalen van n datapunten
 result=$(curl -X GET "http://localhost:4340/metrics?name=test&key=test")
 
 # Use `jq` to verify the result
